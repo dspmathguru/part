5a9e97f7941f255c164059673d0fa42022e93182
Richard J. Tobias <rjjt@cephasonics.com>
2023-03-16 19:09:42 -0500

    [rjjt] first draft BOM import

--- pyPart/importBOM.py
+++ pyPart/importBOM.py
@@ -33,33 +33,45 @@ MFR2 = 'MFR2'
 MPN2 = 'MPN2'
 
 
-def importBOM(bom_file, first_row=0):
+def importBOM(bom_file, name, version, first_row=0):
   db = PartDB.PartDB(db_url)
-  if first_row > 0:
-    bom = pd.read_csv(bom_file, skiprows=first_row-1)
-  else:
-    bom = pd.read_csv(bom_file)
+  bom = BOM(name=name, version=version)
+  db.add(bom)
 
-  bom = bom.dropna(subset=[ITEM, QUANTITY])
-  for i, row in bom.iterrows():
-    print("Importing BOM item %s: %s" % (row[ITEM], row[PART]))
-    part = db.get_part_by_name(row[PART])
+  if first_row > 0:
+    csv = pd.read_csv(bom_file, skiprows=first_row-1)
+  else:
+    csv = pd.read_csv(bom_file)
+
+  csv = csv.dropna(subset=[ITEM, QUANTITY])
+  for i, row in csv.iterrows():
+    print("Importing BOM item %s: %s" % (row[ITEM], row[CEPHA_PN]))
+    part = db.get_part_by_cspnold(row[CEPHA_PN])
     if part is None:
-      print("Part %s not found" % row[PART])
+      print("Part %s not found" % row[CEPHA_PN])
       continue
     print("Found part %s" % part.name)
     print("Adding BOM item %s: %s" % (row[ITEM], row[PART]))
     bom_item = BOMItem(
-        item=row[ITEM],
-        quantity=row[QUANTITY],
-        reference=row[REFERENCE],
+        item_id=bom.id,
+        quantity=int(row[QUANTITY]),
         part=part.id,
-        assembly=row[ASSEMBLY],
-        pcb_footprint=row[PCB_FOOTPRINT],
-        cepha_pn=row[CEPHA_PN],
-        mfr=row[MFR],
-        mpn=row[MPN],
-        mfr2=row[MFR2],
-        mpn2=row[MPN2]
+        reference=row[REFERENCE],
+        assembly=row[ASSEMBLY]
     )
     db.add(bom_item)
+
+
+def main():
+  parser = argparse.ArgumentParser(description='Import BOM from CSV file')
+  parser.add_argument('bom_file', help='BOM CSV file')
+  parser.add_argument('name', help='BOM name')
+  parser.add_argument('version', help='BOM version (e.g. x.y.z))')
+  parser.add_argument('--first_row', type=int,
+                      default=0, help='First row of BOM')
+  args = parser.parse_args()
+  importBOM(args.bom_file, args.name, args.version, args.first_row)
+
+
+if __name__ == '__main__':
+  main()
